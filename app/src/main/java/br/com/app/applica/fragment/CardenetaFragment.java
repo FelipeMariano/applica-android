7e4eda4c804ace0c30eab98545e3bde36490b530
Felipe Mariano <felipemarianodasilva@gmail.com>
2016-11-12 17:29:05 -0200

    Sets warning message to delete menu button

--- app/src/main/java/br/com/app/applica/fragment/CardenetaFragment.java
+++ app/src/main/java/br/com/app/applica/fragment/CardenetaFragment.java
@@ -1,10 +1,12 @@
 package br.com.app.applica.fragment;
 
 
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -14,6 +16,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
@@ -218,9 +221,21 @@ public class CardenetaFragment extends Fragment {
                 System.out.println("I WILL EDIT YOU!");
                 return true;
             case R.id.action_delete:
-                System.out.println("I WILL DELETE YOU!");
+                deleteCardeneta();
                 return true;
         }
+        return false;
+    }
+
+    private Boolean deleteCardeneta(){
+        new AlertDialog.Builder(navActivity).setTitle("Deletar cardeneta")
+                .setMessage("Você realmente deseja deletar a cardeneta permanentemente? Todos os dados aqui serão perdidos!")
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int whichButton){
+                        Toast.makeText(navActivity, "Cardeneta deletada!", Toast.LENGTH_SHORT).show();
+                    }
+                }).setNegativeButton(android.R.string.no, null).show();
         return false;
     }
 
--- app/src/main/java/br/com/app/applica/fragment/HomeFragment.java
+++ app/src/main/java/br/com/app/applica/fragment/HomeFragment.java
@@ -8,6 +8,9 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -128,9 +131,27 @@ public class HomeFragment extends Fragment {
         });
 
         setRecyclerLayout(mRecyclerView);
-
+        setHasOptionsMenu(true);
         return rootView;
     }
+
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        super.onCreateOptionsMenu(menu, inflater);
+        navActivity.getMenuInflater().inflate(R.menu.home_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.action_sync:
+                System.out.println("Sync!");
+                return true;
+        }
+        return false;
+    }
+
 
     private class CardenetasTask extends AsyncTask<Void, Void, User>{
 
