2d5048e02614b4e48ee06718498c32bf62cc8bb4
Felipe Mariano <felipemarianodasilva@gmail.com>
2016-11-20 17:45:05 -0200

    Adds near locations load

--- app/src/main/java/br/com/app/applica/fragment/MapsFragment.java
+++ app/src/main/java/br/com/app/applica/fragment/MapsFragment.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
@@ -33,7 +34,19 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import br.com.app.applica.MainNavActivity;
 import br.com.app.applica.R;
@@ -48,6 +61,7 @@ public class MapsFragment extends Fragment {
     private MainNavActivity navActivity;
     public static List<Unidade> unidades;
     private Boolean LOCATION_MOVE = true;
+    private String AUTH_TOKEN;
 
     private final LatLng LOC = new LatLng(49.187500, -122.849000);
 
@@ -66,6 +80,8 @@ public class MapsFragment extends Fragment {
 
         mMapView.onResume(); //needed to get the map to display immediately;
         navActivity = (MainNavActivity) getActivity();
+
+        AUTH_TOKEN = navActivity.CURRENT_USER.getAuthToken();
 
         setHasOptionsMenu(true);
 
@@ -97,60 +113,20 @@ public class MapsFragment extends Fragment {
                         if(LOCATION_MOVE) {
                             LOCATION_MOVE = false;
 
+                            //getUnidadesNear(location);
+
                          //   myGoogleMap.addMarker(
                           //          new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude()))
                           //                  .title("Minha localização")
                           //                  .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_my_location))
                            // );
 
+                            //addMarkers();
 
                             CameraUpdate update = CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 16);
                             myGoogleMap.animateCamera(update);
 
-                            for (Unidade unidade : unidades) {
-                                List<Float> coordinates = unidade.getLocation().getCoordinates();
-                                LatLng LOC = new LatLng(coordinates.get(0), coordinates.get(1));
-                                String desc = "Endereço: " + unidade.getLocation().getAddress();
-                                desc += "\nTelefone: " + unidade.getTelefone();
-                                desc += "\nE-mail: " + unidade.getEmail();
-                                desc += "\nWebsite: " + unidade.getWebsite();
-
-
-                                myGoogleMap.addMarker(
-                                        new MarkerOptions().position(LOC)
-                                                .title(unidade.getNome())
-                                                .snippet(desc)
-                                                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_default_location))
-                                );
-
-                                myGoogleMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
-                                    @Override
-                                    public View getInfoWindow(Marker marker) {
-                                        return null;
-                                    }
-
-                                    @Override
-                                    public View getInfoContents(Marker marker) {
-                                        LinearLayout info = new LinearLayout(navActivity);
-
-                                        info.setOrientation(LinearLayout.VERTICAL);
-
-                                        TextView title = new TextView(navActivity);
-                                        title.setTextColor(Color.BLACK);
-                                        title.setGravity(Gravity.CENTER);
-                                        title.setTypeface(null, Typeface.BOLD);
-                                        title.setText(marker.getTitle());
-
-                                        TextView snippet = new TextView(navActivity);
-                                        snippet.setTextColor(Color.GRAY);
-                                        snippet.setText(marker.getSnippet());
-
-                                        info.addView(title);
-                                        info.addView(snippet);
-                                        return info;
-                                    }
-                                });
-                            }
+                            getUnidadesNear(location);
                         }
                     }
                 });
@@ -192,4 +168,106 @@ public class MapsFragment extends Fragment {
         }
     }
 
+    private void addMarkers(){
+
+        for (Unidade unidade : unidades) {
+            List<Float> coordinates = unidade.getLocation().getCoordinates();
+            LatLng LOC = new LatLng(coordinates.get(0), coordinates.get(1));
+            String desc = "Endereço: " + unidade.getLocation().getAddress();
+            desc += "\nTelefone: " + unidade.getTelefone();
+            desc += "\nE-mail: " + unidade.getEmail();
+            desc += "\nWebsite: " + unidade.getWebsite();
+
+
+            myGoogleMap.addMarker(
+                    new MarkerOptions().position(LOC)
+                            .title(unidade.getNome())
+                            .snippet(desc)
+                            .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_default_location))
+            );
+
+            myGoogleMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+                @Override
+                public View getInfoWindow(Marker marker) {
+                    return null;
+                }
+
+                @Override
+                public View getInfoContents(Marker marker) {
+                    LinearLayout info = new LinearLayout(navActivity);
+
+                    info.setOrientation(LinearLayout.VERTICAL);
+
+                    TextView title = new TextView(navActivity);
+                    title.setTextColor(Color.BLACK);
+                    title.setGravity(Gravity.CENTER);
+                    title.setTypeface(null, Typeface.BOLD);
+                    title.setText(marker.getTitle());
+
+                    TextView snippet = new TextView(navActivity);
+                    snippet.setTextColor(Color.GRAY);
+                    snippet.setText(marker.getSnippet());
+
+                    info.addView(title);
+                    info.addView(snippet);
+                    return info;
+                }
+            });
+        }
+    }
+
+    private List<Unidade> getUnidadesNear(Location loc){
+        UnidadesNearTask unidadesNearTask = new UnidadesNearTask();
+
+        try{
+            unidadesNearTask.execute(loc.getLatitude(), loc.getLongitude());
+            unidades = unidadesNearTask.get(5000, TimeUnit.MILLISECONDS);
+            System.out.println(unidades);
+            addMarkers();
+        }catch(Exception e){
+            System.out.println("Erro ao puxar unidades perto: " + e);
+        }
+
+        return unidades;
+    }
+
+    private class UnidadesNearTask extends AsyncTask<Double, Void, List<Unidade>>{
+
+        @Override
+        protected List<Unidade> doInBackground(Double... params) {
+            List<Unidade> loadedUnidades = new ArrayList<>();
+
+            Double lat = params[0];
+            Double lng = params[1];
+
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders requestHeaders = new HttpHeaders();
+
+            restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+            requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+            requestHeaders.add("x-access-token", AUTH_TOKEN);
+
+            String url = navActivity.BASE_URL + "/api/locais/nearlocation/";
+
+            UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url)
+                    .queryParam("latitude", lat)
+                    .queryParam("longitude", lng);
+
+            try{
+                HttpEntity<String> httpEntity = new HttpEntity<String>(requestHeaders);
+
+
+                ResponseEntity<List<Unidade>> result = restTemplate.exchange(builder.build().encode().toUri()
+                        , HttpMethod.GET, httpEntity, new ParameterizedTypeReference<List<Unidade>>() {
+                });
+
+                loadedUnidades = result.getBody();
+            }catch(Exception e){
+                System.out.println("UNABLE TO LOAD LOCATIONS: " + e);
+                loadedUnidades = new ArrayList<>();
+            }
+
+            return loadedUnidades;
+        }
+    }
 }
